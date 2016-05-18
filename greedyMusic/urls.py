02bb9985c53be4cb81fe27fe23b6f562613c2d4b
Kumar Shubham <kb4shubham@yahoo.in>
2016-05-19 00:36:29 +0530

    changed urls.py

--- greedyMusic/urls.py
+++ greedyMusic/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^trackmusic/', include('trackMusic.urls')),
+	url(r'^', include('trackMusic.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- trackMusic/urls.py
+++ trackMusic/urls.py
@@ -4,15 +4,15 @@ from . import views
 app_name = 'trackmusic'
 urlpatterns =  [
 	url(r'^$', views.index, name='index'),
-	url(r'^tracks$', views.tracks, name='tracks'),
-	url(r'^tracks/([0-9]+)$', views.trackDetail, name='trackDetail'),
-	url(r'^genres$', views.genres, name='genres'),
-	url(r'^genres/([0-9]+)$', views.genre_detail, name='genre_detail'),
-	url(r'^tracks/add$', views.addTrack, name='addTrack'),
-	url(r'^tracks/edit/([0-9]+)$', views.editTrack, name='editTrack'),
-	url(r'^genres/add$', views.addGenre, name='addGenre'),
-	url(r'^genres/edit/([0-9]+)$', views.editGenre, name='editGenre'),
-    url(r'^tracks/search$', views.searchTracks, name='searchTrack'),
-    url(r'^genres/search$', views.searchGenres, name='searchGenre')
+	url(r'^trackmusic/tracks$', views.tracks, name='tracks'),
+	url(r'^trackmusic/tracks/([0-9]+)$', views.trackDetail, name='trackDetail'),
+	url(r'^trackmusic/genres$', views.genres, name='genres'),
+	url(r'^trackmusic/genres/([0-9]+)$', views.genre_detail, name='genre_detail'),
+	url(r'^trackmusic/tracks/add$', views.addTrack, name='addTrack'),
+	url(r'^trackmusic/tracks/edit/([0-9]+)$', views.editTrack, name='editTrack'),
+	url(r'^trackmusic/genres/add$', views.addGenre, name='addGenre'),
+	url(r'^trackmusic/genres/edit/([0-9]+)$', views.editGenre, name='editGenre'),
+    url(r'^trackmusic/tracks/search$', views.searchTracks, name='searchTrack'),
+    url(r'^trackmusic/genres/search$', views.searchGenres, name='searchGenre')
 
 ]
